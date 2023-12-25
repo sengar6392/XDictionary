07491e933780c34015781696aeed696317d235f3
Shivam Sengar <shivamsengar995627@gmail.com>
2023-12-25 11:00:09 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState("");
+  const [definition, setDefinition] = useState("");
+  const [showDefinition, setShowDefinition] = useState(false);
+  const dictionary = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const handleChange=(e)=>{
+    setShowDefinition(false);
+    const word = e.target.value;
+    setInputText(word.toLowerCase());
+  }
+  const handleSearch = () => {
+    const item = dictionary.find((item)=>item.word.toLowerCase()===inputText);
+    console.log(item);
+    if (item) {
+      setDefinition(item.meaning);
+    }else{
+      setDefinition(null)
+    }
+    setShowDefinition(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Dictionary App</h1>
+      <input
+        type="text"
+        placeholder="Search for a word..."
+        value={inputText}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <h4>Definition:</h4>
+      {showDefinition && (
+        <div>
+          {definition ? definition : "Word not found in the dictionary."}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
